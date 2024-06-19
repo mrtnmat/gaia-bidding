be8b255d651e6301f9bcade6dcd25dea602df4bd
Matteo Martini <mrtn.matteo@proton.me>
2024-06-19 17:33:47 +0200

    basic layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,53 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { randInt } from './shared/utils';
-import { Color, randomDistinctColors } from './shared/models/color.enum';
-import { factionsOfColor, randomFactions } from './shared/models/faction-data';
+import { Color, colorClass, randomDistinctColors } from './shared/models/color.enum';
+import { factionData, factionsOfColor, randomFactions } from './shared/models/faction-data';
+import { CommonModule } from '@angular/common';
+import { Faction } from './shared/models/faction.enum';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'gaia-bidding';
+
+  private playerCount: number = 3
+  private readonly _factions: Faction[] = randomFactions(this.playerCount)
+
+  colors = Color
+
+  readonly players = [
+    this.setupPlayer('Tim'),
+    this.setupPlayer('John'),
+    this.setupPlayer('Alice'),
+  ]
+
+  setupPlayer(name: string) {
+    let bids = this._factions.map((v) => {
+      return {
+        faction: v,
+        value: 0
+      }
+    })
+    return { name, bids }
+  }
+
+
+  readonly factions = this._factions.map((v) => {
+    let color = factionData.get(v)?.color
+    return {
+      name: v,
+      bid: -1,
+      player: undefined,
+      color,
+      colorClass: colorClass(color!),
+    }
+  })
 
   randInt() {
     console.log(randomDistinctColors(6))
--- src/app/shared/models/color.enum.ts
+++ src/app/shared/models/color.enum.ts
@@ -12,6 +12,25 @@ export enum Color {
     White = 'white'
 }
 
+export function colorClass(c: Color): string {
+    switch (c) {
+        case Color.Blue:
+            return 'bg-blue-500'
+        case Color.Yellow:
+            return 'bg-yellow-500'
+        case Color.Brown:
+            return 'bg-amber-800'
+        case Color.Red:
+            return 'bg-red-500'
+        case Color.Orange:
+            return 'bg-orange-500'
+        case Color.Gray:
+            return 'bg-stone-400'
+        default:
+            return 'bg-white'
+    }
+}
+
 export function randomDistinctColors(n: number): Color[] {
     if (!Number.isInteger) throw new ArgumentException(`${n} is not an integer`)
     if (n < 0) throw new ArgumentException(`Argument must be a positive integer`)
